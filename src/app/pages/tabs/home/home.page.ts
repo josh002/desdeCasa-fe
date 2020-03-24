4845ed20c10eb6d67047246700fd877b34cfc692
burgardt <german@ahoradoctor.com>
2020-03-24 19:24:26 -0300

    getcommercesbyuser implementado en home

--- src/app/pages/tabs/home/home.page.ts
+++ src/app/pages/tabs/home/home.page.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from 'src/app/services/localStorageService';
+import { AuthService } from 'src/app/services/authService';
+import { Commerce } from 'src/app/models/commerce.model';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
+    selector: 'app-home',
+    templateUrl: './home.page.html',
+    styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+    commerces: any;
 
-  ngOnInit() {
-  }
+    constructor(
+        private localStorageService: LocalStorageService,
+        private authService: AuthService
+    ) { }
+
+    ngOnInit() {
+        // const userId = this.localStorageService.getObject('userId');
+        const userId = 13;
+
+        this.authService.getCommercesByUser(userId)
+            .then(
+                commerces => {
+                    // debugger;
+                    this.commerces = commerces
+                }
+            )
+    }
 
 }
