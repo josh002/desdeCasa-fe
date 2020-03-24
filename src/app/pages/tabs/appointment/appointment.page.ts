43e7191dc3115f34b15d32d503ded7b13e9ae506
Josue Carrasco <jos247car@gmail.com>
2020-03-24 17:59:13 -0300

    pantalla empezar , inicado

--- src/app/pages/tabs/appointment/appointment.page.ts
+++ src/app/pages/tabs/appointment/appointment.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-appointment',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppointmentPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public alertController: AlertController,
+  ) { }
 
   ngOnInit() {
+  }
+  async myAppointment() {
+    const alert = await this.alertController.create({
+      header: 'Turno en supermercado',
+      subHeader: 'Don Pepe',
+      message: 'Horario : 8:15',
+      buttons: ['Cancelar Turno', 'OK']
+    });
+    await alert.present();
   }
 
 }
