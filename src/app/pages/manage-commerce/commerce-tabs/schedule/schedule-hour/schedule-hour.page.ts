0084b368661cce3cef7b20ff378c10b1580aed62
Josue Carrasco <jos247car@gmail.com>
2020-03-25 11:33:48 -0300

    detalles de cliente mas pop up terminado

--- src/app/pages/manage-commerce/commerce-tabs/schedule/schedule-hour/schedule-hour.page.ts
+++ src/app/pages/manage-commerce/commerce-tabs/schedule/schedule-hour/schedule-hour.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-schedule-hour',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScheduleHourPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public alertController: AlertController,
+  ) { }
 
   ngOnInit() {
   }
-
+  async myAppointment() {
+    const alert = await this.alertController.create({
+      header: 'Turno en supermercado',
+      subHeader: 'Don Pepe',
+      message: 'Horario : 8:15',
+      buttons: ['Cancelar Turno', 'OK']
+    });
+    await alert.present();
+  }
 }
