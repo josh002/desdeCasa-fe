1e4f08c553cd03c5088ad9e6fe176e76f88130ed
emiliopsy <44709555+emiliopsy@users.noreply.github.com>
2020-03-25 18:55:27 -0300

    trabanjo logout

--- src/app/pages/manage-user/tabs/logout/logout.page.ts
+++ src/app/pages/manage-user/tabs/logout/logout.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Client } from 'src/app/models/client.model';
+import { LocalStorageService } from 'src/app/services/localStorageService';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-logout',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogoutPage implements OnInit {
 
-  constructor() { }
+  email: string;
+  password: string;
+  client: Client;
+
+
+  constructor(
+    private localStorageService: LocalStorageService,
+  ) {
+
+  }
 
   ngOnInit() {
+
   }
+
+  // Gotcha!
+  ionViewWillEnter() {
+    this.password = undefined;
+    console.log('COnsoleame algo FORRO!');
+    console.log('local storage', this.localStorageService.getObject('client'));
+    console.log('newClient', new Client(this.localStorageService.getObject('client')));
+    this.client = new Client(this.localStorageService.getObject('client'));
+    console.log('this.client', this.client)
+
+  }
+
+  doLogOut() {
+    this.localStorageService.clearLocalStorage();
+    // this.router.navigateByUrl('/start');
+  }
+
 
 }
