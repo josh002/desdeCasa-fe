3f519afe1642d0b2b2b77d3eb687b503f7e942c4
cris1ian <cristian_rosarino@live.com.ar>
2020-03-26 03:17:32 -0300

    Reapuntado al server de Eon6

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
     production: false,
-    WS_URL: 'http://localhost:3001/api/v1',
-    // WS_URL: 'http://ec2-3-80-143-174.compute-1.amazonaws.com:3001/api/v1',
+    // WS_URL: 'http://localhost:3001/api/v1',
+    WS_URL: 'http://ec2-3-80-143-174.compute-1.amazonaws.com:3001/api/v1',
     georef_ar_api: 'https://apis.datos.gob.ar/georef/api'
 };
 
