b2a833d535d90ae9ed96d7ab230a39f7f770a96e
Josue Carrasco <jos247car@gmail.com>
2020-03-27 14:11:19 -0300

    comente la barra de busqueda, se cambio la url del servidor

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  WS_URL: 'http://ec2-3-80-143-174.compute-1.amazonaws.com:3001/api/v1',
+  WS_URL: 'http://54.172.118.239:3001/api/v1',
   //   WS_URL: 'SERVER MINISTERIO DE DEFENSA',
   georef_ar_api: 'https://apis.datos.gob.ar/georef/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
     production: false,
     // WS_URL: 'http://localhost:3001/api/v1',
-    WS_URL: 'http://ec2-3-80-143-174.compute-1.amazonaws.com:3001/api/v1',
+    WS_URL: 'http://54.172.118.239:3001/api/v1',
     georef_ar_api: 'https://apis.datos.gob.ar/georef/api'
 };
 
