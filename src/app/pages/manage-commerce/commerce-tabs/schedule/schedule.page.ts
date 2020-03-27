f84cc214bd6e42c1c54014238bd9c6d4c22f960e
emiliopsy <44709555+emiliopsy@users.noreply.github.com>
2020-03-26 22:21:22 -0300

    pantalla de turnos comercio

--- src/app/pages/manage-commerce/commerce-tabs/schedule/schedule.page.ts
+++ src/app/pages/manage-commerce/commerce-tabs/schedule/schedule.page.ts
@@ -5,6 +5,7 @@ import { Commerce } from 'src/app/models/commerce.model';
 import { Booking } from 'src/app/models/booking.model';
 import { AlertService } from 'src/app/services/alertService';
 import { Router } from '@angular/router';
+import { asDate } from 'src/app/constants/constants';
 
 @Component({
     selector: 'app-schedule',
@@ -12,10 +13,16 @@ import { Router } from '@angular/router';
     styleUrls: ['./schedule.page.scss'],
 })
 export class SchedulePage implements OnInit {
-    bookings: any[];
+    // bookings: any[];
     commerce: Commerce;
 
-    list = [7, 9, 11, 13, 15, 17, 19, 21, 22, 23];
+    startHour1: number;
+    endHour1: number;
+    startHour2: number;
+    endHour2: number;
+
+    // Arreglo con horas
+    hours: number[];
 
     constructor(
         private authService: AuthService,
@@ -27,20 +34,37 @@ export class SchedulePage implements OnInit {
     ngOnInit() { };
 
     ionViewWillEnter() {
+        this.hours = [];
+        for (var i = 0; i < 24; i++) this.hours.push(i);
         this.commerce = new Commerce(this.localStorageService.getObject('commerce'));
-        this.authService.getBookingsByCommerce(this.commerce.id)
-            .then((resp: any) => {
-                console.log('commerce', this.commerce);
-                this.bookings = [];
-                resp.result.forEach(elem => this.bookings.push(new Booking(elem)));
-                console.log('bookings', this.bookings)
-            })
-            .catch(err => {
-                console.log('err', err);
-                this.alertService.simpleAlert("Ocurrió un error inesperado. Intente más tarde.");
-            });
+
+        this.startHour1 = asDate(this.commerce.openTime1).getHours();
+        this.endHour1 = asDate(this.commerce.closeTime1).getHours();
+        this.startHour2 = this.commerce.openTime2 === null ? undefined : asDate(this.commerce.openTime2).getHours();
+        this.endHour2 = this.commerce.closeTime2 === null ? undefined : asDate(this.commerce.closeTime2).getHours();
+        if (asDate(this.commerce.closeTime1).getMinutes() > 0) this.endHour1++;
+        if (asDate(this.commerce.closeTime2).getMinutes() > 0) this.endHour2++;
+
+        // this.route.paramMap.subscribe(
+        //     (params: Params) => {
+        //         const hour: number = params.params.id;
+
+        //         this.authService.getBookingsByCommerce(this.commerce.id)
+        //             .then((resp: any) => {
+        //                 console.log('commerce', this.commerce);
+        //                 this.bookings = [];
+        //                 resp.result.forEach(elem => this.bookings.push(new Booking(elem)));
+        //                 // this.bookings = this.bookings.filter(elem => elem.description == hour);
+        //                 console.log('bookings', this.bookings)
+        //             })
+        //             .catch(err => {
+        //                 console.log('err', err);
+        //                 this.alertService.simpleAlert("Ocurrió un error inesperado. Intente más tarde.");
+        //             });
+
+        //     }
+        // )
+
 
     }
-
-
 }
