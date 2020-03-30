0d69ff5fa7ab8bd552901f4abff64e6c5c76bcf2
cris1ian <cristian_rosarino@live.com.ar>
2020-03-29 23:20:36 -0300

    Production server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    WS_URL: 'http://54.172.118.239:3001/api/v1',         // Eon6
+    WS_URL: 'http://200.55.243.179:3001/api/v1',         // Production Server
     georef_ar_api: 'https://apis.datos.gob.ar/georef/api'
 };
