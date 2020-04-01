216edb57509a4ee33aa4493cd48d8408ba69d82f
Josue Carrasco <jos247car@gmail.com>
2020-04-01 17:30:51 -0300

    cambie routing y funcionamiento de las pantallas de comercio

--- src/app/pages/manage-commerce/commerce-tabs/commerce-tabs-routing.module.ts
+++ src/app/pages/manage-commerce/commerce-tabs/commerce-tabs-routing.module.ts
@@ -4,30 +4,48 @@ import { Routes, RouterModule } from '@angular/router';
 import { CommerceTabsPage } from './commerce-tabs.page';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: CommerceTabsPage,
-    children:[
-        {
-            path: 'schedule',
-            loadChildren: () => import('./schedule/schedule.module').then( m => m.SchedulePageModule)
-          },
-          {
-            path: 'edit-commerce',
-            loadChildren: () => import('./edit-commerce/edit-commerce.module').then( m => m.EditCommercePageModule)
-          },
-          {
-            path: 'logout',
-            loadChildren: () => import('./logout/logout.module').then( m => m.LogoutPageModule)
-          },
-    ]
-  },
-  
+    {
+        path: '',
+        component: CommerceTabsPage,
+        children: [
+            {
+                path: 'schedule',
+                children: [
+                    {
+                        path: '',
+                        loadChildren: () => import('./schedule/schedule.module').then(m => m.SchedulePageModule)
+                    },
+                    {
+                        path: 'logout',
+                        loadChildren: () => import('./logout/logout.module').then(m => m.LogoutPageModule)
+                    },
+
+                ]
+
+            },
+            {
+                path: 'edit-commerce',
+                children: [
+                    {
+                        path: '',
+                        loadChildren: () => import('./edit-commerce/edit-commerce.module').then(m => m.EditCommercePageModule)
+                    },
+                    {
+                        path: 'logout',
+                        loadChildren: () => import('./logout/logout.module').then(m => m.LogoutPageModule)
+                    },
+                ]
+
+            },
+
+        ]
+    },
+
 
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
-export class CommerceTabsPageRoutingModule {}
+export class CommerceTabsPageRoutingModule { }
--- src/app/pages/manage-commerce/commerce-tabs/schedule/schedule-routing.module.ts
+++ src/app/pages/manage-commerce/commerce-tabs/schedule/schedule-routing.module.ts
@@ -4,18 +4,29 @@ import { Routes, RouterModule } from '@angular/router';
 import { SchedulePage } from './schedule.page';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: SchedulePage
-  },
-  {
-    path: 'schedule-hour/:id',
-    loadChildren: () => import('./schedule-hour/schedule-hour.module').then(m => m.ScheduleHourPageModule)
-  }
+    {
+        path: '',
+        component: SchedulePage
+    },
+    {
+        path: 'schedule-hour/:id',
+        children: [
+            {
+                path: '',
+                loadChildren: () => import('./schedule-hour/schedule-hour.module').then(m => m.ScheduleHourPageModule)
+            },
+            {
+                path: 'logout',
+                loadChildren: () => import('../logout/logout.module').then(m => m.LogoutPageModule)
+            },
+        ]
+
+    },
+
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
 export class SchedulePageRoutingModule { }
